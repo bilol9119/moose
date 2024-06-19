11a759c6bf7c02452f9b3301137362d70e2af76c
bilol <bilolnorov7@gmail.com>
2024-06-19 17:44:47 +0500

    allowed hosts all

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mjkjm$21#)$sx2#k&1(t1iwypk%7vj=a(c*xti9o*755zjk7@n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*', ]
 
 
 # Application definition
@@ -117,10 +117,10 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR/'static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
