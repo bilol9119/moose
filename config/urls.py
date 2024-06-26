72ce14d1eacb3aebd36b224822c4ba3932dcff73
bilol <bilolnorov7@gmail.com>
2024-06-26 18:34:16 +0500

    allowed hosts all

--- config/urls.py
+++ config/urls.py
@@ -7,11 +7,8 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    re_path(r'^static/(?P<path>.*)$', serve),
-    re_path(
-        r'^media/(?P<path>.*)$', serve,
-        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}
-    ),
+    re_path(r'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
 
 ]
 
